6de3472571cfa14cf6239ddf5827e894552044fb
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-01-14 20:27:44 -0700

    Add project files.

---
+++ Lab0/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+/*
+ *CPRG 211 Lab 0 Activity
+ * Author: Karina Chiste
+ * When: Winter 2025
+ */
+
+// get low and high int numbers
+//low must be positive
+// high must be greater than low
+
+using Lab0;
+
+int low, high;
+int diff; // difference between low and high
+
+low = Utilities.GetPositiveInt("low number");
+
+high = Utilities.GetIntInRange("high number", low + 1, Int32.MaxValue);
+
+diff = high - low;
+
+Console.WriteLine($"The low number: {low}");
+Console.WriteLine($"The high number: {high}");
+
+Console.WriteLine($"The difference between {low} and {high} is {diff}");
+
+
+// create an array to hold numbers between low and high
+int[] numbers = new int[diff + 1]; // size of the array
+for (int i = 0; i<= diff; i++) // i is the index 
+{
+    numbers[i] = low + i;
+}
+
+Console.WriteLine("Numbers in the array");
+for (int i = 0;i<= diff; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+//create a file name "number.txt" and write to it the numbers from the array in reverse order
+StreamWriter streamWriter = File.CreateText("numbers.txt"); // located in the same folder as the .exe file
+for (int i = numbers.Length - 1; i >= 0; i--)
+{
+    streamWriter.WriteLine(numbers[i]);
+}
+streamWriter.Close(); // important to finalize the writing
+Console.WriteLine("File Written");
---
+++ Lab0/Utilities.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    /// <summary>
+    /// Methods for getting and validating input data and relevant calculation
+    /// </summary>
+    internal static class Utilities
+    {
+        /// <summary>
+        /// reads positive decimal
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <returns>decimal value read that is >0</returns>
+        public static decimal GetPositiveDecimal(string prompt)
+        {
+            bool gotIt = false;
+            decimal result = 0;
+            string input;
+
+            while (!gotIt) //while not gotIt (while gotIt is false)
+            {
+                Console.WriteLine($"Enter {prompt}:"); //interpolated string
+                input = Console.ReadLine() ?? ""; // null coalescing operator
+                // TryParse attempts to parse the string first argument
+                // returns false (instead of crashing) when cannot do
+                //returns true and assigns the second arg variable if successful
+                if (Decimal.TryParse(input, out result))  // result is assigned the parsed value
+                {
+                    if (result > 0)
+                    {
+                        gotIt = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {prompt} must be positive");
+                    }
+                }
+                else // could not parse
+                {
+                    Console.WriteLine($"Error: {prompt} must be a number");
+                }
+            }
+
+
+            return result;
+        }
+
+    
+
+      /// <summary>
+        /// reads positive decimal
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <returns>decimal value read that is >0</returns>
+        public static int GetPositiveInt(string prompt)
+        {
+            bool gotIt = false;
+            int result = 0;
+            string input;
+
+            while (!gotIt) //while not gotIt (while gotIt is false)
+            {
+                Console.WriteLine($"Enter {prompt}:"); //interpolated string
+                input = Console.ReadLine() ?? ""; // null coalescing operator
+                // TryParse attempts to parse the string first argument
+                // returns false (instead of crashing) when cannot do
+                //returns true and assigns the second arg variable if successful
+                if (Int32.TryParse(input, out result))  // result is assigned the parsed value
+                {
+                    if (result > 0)
+                    {
+                        gotIt = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {prompt} must be positive");
+                    }
+                }
+                else // could not parse
+                {
+                    Console.WriteLine($"Error: {prompt} must be a whole number");
+                }
+            }
+
+
+            return result;
+        }
+
+        /// <summary>
+        /// reads positive decimal
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <returns>decimal value read that is >0</returns>        
+        public static int GetIntInRange(string prompt, int minValue, int maxValue) {
+            bool gotIt = false;
+            int result = 0;
+            string input;
+
+            while (!gotIt) //while not gotIt (while gotIt is false)
+            {
+                Console.WriteLine($"Enter {prompt}:"); //interpolated string
+                input = Console.ReadLine() ?? ""; // null coalescing operator
+                // TryParse attempts to parse the string first argument
+                // returns false (instead of crashing) when cannot do
+                //returns true and assigns the second arg variable if successful
+                if (Int32.TryParse(input, out result))  // result is assigned the parsed value
+                {
+                    if (result >= minValue && result <= maxValue)
+                    {
+                        gotIt = true;
+                    }
+                    else
+                    {
+                        if (minValue == Int32.MinValue)
+                            Console.WriteLine($"Error: {prompt} must be positive");
+                        if (maxValue <= minValue)
+                            Console.WriteLine();
+                    }
+                }
+                else // could not parse
+                {
+                    Console.WriteLine($"Error: {prompt} must be a whole number");
+                }
+            }
+
+
+            return result;
+        } }
+    }
